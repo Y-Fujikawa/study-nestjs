f930b59d2cb6b5b9e10c8af777df8daff71ec8d5
Y_Fujikawa <fujikawa0@gmail.com>
2020-12-24 11:50:53 +0900

    ジェネレートコマンドを使ったコントローラ追加を試した

---
+++ src/cats/cats.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CatsController } from './cats.controller';
+
+describe('CatsController', () => {
+  let controller: CatsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CatsController],
+    }).compile();
+
+    controller = module.get<CatsController>(CatsController);
+  });
+
+  it('should return "Hello World! Nest.js!!"', () => {
+    expect(controller.findAll()).toBe('This action returns all cats');
+  });
+});
